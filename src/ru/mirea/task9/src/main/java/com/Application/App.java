f2a45eaabcb4f7d5c0c1d3db08d683f5afbbd2f8
Pavel <zhuravlev.p.r@edu.mirea.ru>
2022-03-29 10:11:28 +0300

    Feat: add Task 9

---
+++ src/ru/mirea/task9/src/main/java/com/Application/App.java
@@ -0,0 +1,8 @@
+package ru.mirea.task9.src.main.java.com.Application;
+
+public class App {
+    public static void main(String[] args){
+        System.out.println("Print something...");
+        return;
+    }
+}

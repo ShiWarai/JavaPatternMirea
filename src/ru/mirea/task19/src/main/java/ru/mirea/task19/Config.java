b2a8f6e9d8ed4477333b756ca784a20989e3745d
Pavel <zhuravlev.p.r@edu.mirea.ru>
2022-05-06 09:35:08 +0300

    Feat: add Task 19

---
+++ src/ru/mirea/task19/src/main/java/ru/mirea/task19/Config.java
@@ -0,0 +1,47 @@
+package ru.mirea.task19;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+@EnableJpaRepositories(basePackages = "ru.mirea.task19.repositories")
+public class Config {
+//
+//    @Bean
+//    public HikariDataSource dataSource() {
+//        HikariConfig config = new HikariConfig();
+//        config.setDriverClassName("org.postgresql.Driver");
+//        config.setJdbcUrl("jdbc:postgresql://localhost:5432/postgres");
+//        config.setUsername("shiwarai");
+//        config.setPassword("shiwarai");
+//        return new HikariDataSource(config);
+//    }
+//
+//    @Bean
+//    public LocalSessionFactoryBean sessionFactoryBean(DataSource dataSource) {
+//        LocalSessionFactoryBean sessionFactoryBean = new LocalSessionFactoryBean();
+//        sessionFactoryBean.setDataSource(dataSource);
+//
+//        sessionFactoryBean.setPackagesToScan("ru.mirea.task19");
+//        Properties properties = new Properties();
+//        properties.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQL10Dialect");
+//        sessionFactoryBean.setHibernateProperties(properties);
+//        return sessionFactoryBean;
+//    }
+//
+//    @Bean
+//    public PlatformTransactionManager platformTransactionManager(LocalSessionFactoryBean factoryBean) {
+//        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
+//        transactionManager.setSessionFactory(factoryBean.getObject());
+//        return transactionManager;
+//    }
+}
